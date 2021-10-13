98aab109879d21ff737d6c42b46a24c04efc16ed
VucCPaK <kirilogrechka@gmail.com>
2021-10-13 21:17:43 +0300

    add roles

---
+++ frontend/src/main/app/components/users/admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link class="a_left" to="/">HOME</router-link>
+    <router-link class="a_left" to="/shop">SHOP</router-link>
+    <router-link class="a_right" to="/cart">CART</router-link>
+    <router-link class="a_right" to="/cabinet">CABINET</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "admin"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/main/app/components/users/anonymous.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link class="a_left" to="/">HOME</router-link>
+    <router-link class="a_left" to="/shop">SHOP</router-link>
+    <a class="a_right" href="http://localhost:8085/auth/realms/my_realm/protocol/openid-connect/auth?client_id=my_client&response_type=code&redirect_uri=http://localhost:8082">SIGN IN</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "anonymous"
+}
+</script>
+
+<style scoped>
+
+</style>
